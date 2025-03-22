88793afd56749cda6c2dcc21a7e26ffb77ebc368
Marina Duarte Cabral <marinaduartcabral@gmail.com>
2025-03-22 00:28:14 +0000

    Código testado

---
+++ aula4/target/debug/deps/aula4-a81676f3c69c0161.d
@@ -0,0 +1,5 @@
+/workspaces/marina.cabral.3na/aula4/target/debug/deps/aula4-a81676f3c69c0161: src/lib.rs
+
+/workspaces/marina.cabral.3na/aula4/target/debug/deps/aula4-a81676f3c69c0161.d: src/lib.rs
+
+src/lib.rs:
